9d27f43d5a1246966758c5613e3ca783f7511580
Tim Cross <theophilusx@gmail.com>
2018-11-16 15:46:08 +1100

    Update index.js to use new modules

--- src/index.js
+++ src/index.js
@@ -2,25 +2,48 @@
 
 const config = require("./config");
 const github = require("./github");
+const packages = require("./packages");
 
 const rc = config.getConfig(".github-npm-report.json");
 const orgUnit = process.argv[2];
+const repoName = process.argv[3];
+const branch = process.argv[4];
+
+async function buildPackageLists(orgName, repoName, packageFiles) {
+  let depSet = new Set();
+  let devDepSet = new Set();
+  
+  for (let [name, sha] of packageFiles) {
+    if (name.match(/package.json/)) {
+      console.log(`Processing ${name}`);
+      let pkgObj = await github.getBlob(orgName, repoName, sha);
+      let [dep, dev] = packages.parsePackageJsonObj(pkgObj);
+      for (let i of dep.values()) {
+        depSet.add(i);
+      }
+      for (let i of dev.values()) {
+        devDepSet.add(i);
+      }
+    }
+  }
+  return [depSet, devDepSet];
+}
 
 github.init(rc);
 
-github.getOrganizationRepos(orgUnit)
-  .then(repos => {
-    console.dir(repos);
-    return github.findPackageJson("SheepCRC", "weatherdb", "master");
-  })
+github.findPackageJson(orgUnit,repoName, branch)
   .then(files => {
-    console.dir(files);
+    return buildPackageLists(orgUnit, repoName, files);
+  })
+  .then(([dep, dev]) => {
+    return packages.packageVersions(dep);
+  })
+  .then(pInfo => {
+    console.dir(pInfo);
   })
   .catch(err => {
     console.error(err.message);
   });
-
-
 
 
 
