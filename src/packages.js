0c1876980a705c176cc392578ca073aedcff1292
Tim Cross <theophilusx@gmail.com>
2019-02-19 16:02:09 +1100

    Refactoring of package parsing

--- src/packages.js
+++ src/packages.js
@@ -1,86 +1,99 @@
 "use strict";
 
+const moduleName = "packages";
+
 const VError = require("verror");
 const latestVersion = require("latest-version");
 const semver = require("semver");
-
-function parsePackageJsonObj(pkgObj, pkgInfo, depType = "dependencies") {
-  try {
-    let pkgName = pkgObj.name || "Unknown";
-    let pkgVersion = pkgObj.version || "0.0.0";
-    let pkgDesc = pkgObj.description || "None";
-    let deps = pkgObj[depType] || {};
-    for (let k of Object.keys(deps)) {
-      let version = semver.valid(semver.coerce(deps[k]));
-      if (version) {
-        let coordinate = `pkg:npm/${k}@${version}`.toLowerCase();
-        if (pkgInfo.has(coordinate)) {
-          pkgInfo.get(coordinate).usedBy.push({
-            name: pkgName,
-            version: pkgVersion,
-            description: pkgDesc
-          });
-        } else {
-          pkgInfo.set(coordinate, {
-            usedBy: [
-              {
-                name: pkgName,
-                version: pkgVersion,
-                description: pkgDesc
-              }
-            ]
-          });
-        }
-      } else {
-        console.log(`Core package with bad version: ${k}/${deps[k]}`);
-      }
-    }
-    return pkgInfo;
-  } catch (err) {
-    throw new VError(err, `Failed to process package ${pkgObj.name}`);
-  }
-}
 
 function nextMajor(v) {
   let m = parseInt(v.split(".")[0]) + 1;
   return `${m}.0.0`;
 }
 
-async function getVersionInfo(name, version) {
+async function getVersionInfo(name, ver) {
   const logName = "getVersionInfo";
 
   try {
-    let latest = await latestVersion(name);
-    let majorVersion = `<${nextMajor(version)}`;
-    let majorLatest = await latestVersion(name, {version: majorVersion});
+    let version = semver.valid(semver.coerce(ver));
+    if (version) {
+      let latest = await latestVersion(name);
+      let majorVersion = `<${nextMajor(version)}`;
+      let majorLatest = await latestVersion(name, {version: majorVersion});
+      return {
+        current: version,
+        next: majorLatest,
+        latest: latest
+      };
+    }
+    console.log(`${logName} Package with bad version: ${name}@${ver}`);
     return {
-      current: version,
-      next: majorLatest,
-      latest: latest
+      current: ver,
+      next: "Unknown",
+      latest: "Unknown"
     };
   } catch (err) {
     console.error(`${logName}: ${err.message}`);
     return {
-      current: version,
-      next: "unknown",
-      latest: "unknon"
+      current: ver,
+      next: "Unknown",
+      latest: "Unknon"
     };
   }
 }
 
-async function packageVersions(packageInfo) {
-  for (let e of packageInfo.keys()) {
-    let [name, version] = e.split("/")[1].split("@");
-    let versionInfo = getVersionInfo(name, version);
-    packageInfo.get(e).current = versionInfo.current;
-    packageInfo.get(e).next = versionInfo.next;
-    packageInfo.get(e).latest = versionInfo.latest;
+async function parsePackageJSON(pkgObj, packages) {
+  const logName = `${moduleName}.parsePackageJSON`;
+
+  try {
+    let versionInfo = await getVersionInfo(pkgObj.name, pkgObj.version);
+    console.dir("versionInfo", versionInfo);
+    let currentPkg = {
+      name: pkgObj.name,
+      current: versionInfo.current,
+      next: versionInfo.next,
+      latest: versionInfo.latest,
+      description: pkgObj.description || "Unknown",
+      usedBy: []
+    };
+    let coords = `pkg:npm/${currentPkg.name}@${
+      currentPkg.current
+    }`.toLowerCase();
+    if (!packages.has(coords)) {
+      packages.set(coords, currentPkg);
+    } else {
+      packages.get(coords).description = pkgObj.description;
+    }
+    for (let d of ["dependencies", "devDependencies"]) {
+      let dep = pkgObj[d];
+      for (let k of Object.keys(dep)) {
+        versionInfo = await getVersionInfo(k, dep[k]);
+        coords = `pkg:npm/${k}@${versionInfo.current}`.toLowerCase();
+        if (packages.has(coords)) {
+          packages
+            .get(coords)
+            .usedBy.push([currentPkg.name, currentPkg.current]);
+        } else {
+          packages.set(coords, {
+            name: k,
+            current: versionInfo.current,
+            next: versionInfo.next,
+            latest: versionInfo.latest,
+            usedBy: [[currentPkg.name, currentPkg.version]]
+          });
+        }
+      }
+    }
+    return packages;
+  } catch (err) {
+    throw new VError(
+      err,
+      `${logName} Failed to process package ${pkgObj.name}`
+    );
   }
-  return packageInfo;
 }
 
 module.exports = {
-  parsePackageJsonObj,
   getVersionInfo,
-  packageVersions
+  parsePackageJSON
 };
